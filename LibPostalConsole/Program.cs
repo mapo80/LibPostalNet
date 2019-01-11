a43b7c4f89fd8f033deb454c55c5ea981517468a
Michael Evans <aimisterrible@gmail.com>
2019-01-11 14:07:08 +0000

    Adding logic for using expander

--- LibPostalConsole/Program.cs
+++ LibPostalConsole/Program.cs
@@ -12,18 +12,49 @@ namespace LibPostalConsole
     {
         static void Main(string[] args)
         {
-            libpostal.LibpostalSetup();
-            libpostal.LibpostalSetupParser();
+            if (args.Length > 0)
+            {
+                var dataPath = args[0];
+                libpostal.LibpostalSetupDatadir(dataPath);
+                libpostal.LibpostalSetupParserDatadir(dataPath);
+                libpostal.LibpostalSetupLanguageClassifierDatadir(dataPath);
+            }
+            else
+            {
+                libpostal.LibpostalSetup();
+                libpostal.LibpostalSetupParser();
+                libpostal.LibpostalSetupLanguageClassifier();
+            }
 
-            var response = libpostal.LibpostalParseAddress("781 Franklin Ave Crown Heights Brooklyn NYC NY 11216 USA", new LibpostalAddressParserOptions());
+            var query = "Av. Beira Mar 1647 - Salgueiros, 4400-382 Vila Nova de Gaia";
+
+
+            var response = libpostal.LibpostalParseAddress(query, new LibpostalAddressParserOptions());
 
             var x = response.Results;
+            foreach (var result in x)
+            {
+                Console.WriteLine(result.ToString());
+            }
+
+            unsafe
+            {
+                ulong n = ulong.MinValue;
+                var expansions = libpostal.LibpostalExpandAddress(query, libpostal.LibpostalGetDefaultOptions(), ref n);
+                for (ulong i = 0; i < n; i++)
+                {
+                    var normalized = new IntPtr(expansions[i]);
+                    var str = Marshal.PtrToStringAnsi(normalized);
+                    Console.WriteLine(str);
+                }
+            }
 
             libpostal.LibpostalAddressParserResponseDestroy(response);
 
             // Teardown (only called once at the end of your program)
             libpostal.LibpostalTeardown();
             libpostal.LibpostalTeardownParser();
+            libpostal.LibpostalTeardownLanguageClassifier();
         }
     }
 }
