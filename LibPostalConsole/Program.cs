f275048b0213230f9cbfc28febe8150d3a461e63
Floris Westerman <me@floriswesterman.nl>
2020-05-07 22:02:28 +0200

    Fix permissions, clean up

--- LibPostalConsole/Program.cs
+++ LibPostalConsole/Program.cs
@@ -6,37 +6,48 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibPostalConsole {
-    internal class Program {
+namespace LibPostalConsole
+{
+    class Program
+    {
         /// <summary>
         /// An example of using the LibPostalNet library
         /// </summary>
         /// <param name="args">args[0] should be the path to libpostal data files</param>
-        private static void Main(string[] args) {
-            string dataPath = Console.ReadLine();
-            libpostal.LibpostalSetupDatadir(dataPath);
-            libpostal.LibpostalSetupParserDatadir(dataPath);
-            libpostal.LibpostalSetupLanguageClassifierDatadir(dataPath);
-
-            var query = "";
-            while (!string.IsNullOrWhiteSpace(query = Console.ReadLine())) {
-                LibpostalAddressParserResponse response =
-                    libpostal.LibpostalParseAddress(query, new LibpostalAddressParserOptions());
-
-                List<KeyValuePair<string, string>> x = response.Results;
-                foreach (KeyValuePair<string, string> result in x) {
-                    Console.WriteLine(result.ToString());
-                }
-
-                libpostal.LibpostalAddressParserResponseDestroy(response);
-
-                LibpostalNormalizeResponse expansion =
-                    libpostal.LibpostalExpandAddress(query, libpostal.LibpostalGetDefaultOptions());
-                foreach (string s in expansion.Expansions) {
-                    Console.WriteLine(s);
-                }
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var dataPath = args[0];
+                libpostal.LibpostalSetupDatadir(dataPath);
+                libpostal.LibpostalSetupParserDatadir(dataPath);
+                libpostal.LibpostalSetupLanguageClassifierDatadir(dataPath);
+            }
+            else
+            {
+                libpostal.LibpostalSetup();
+                libpostal.LibpostalSetupParser();
+                libpostal.LibpostalSetupLanguageClassifier();
             }
 
+            var query = "Av. Beira Mar 1647 - Salgueiros, 4400-382 Vila Nova de Gaia";
+
+            var response = libpostal.LibpostalParseAddress(query, new LibpostalAddressParserOptions());
+
+            var x = response.Results;
+            foreach (var result in x)
+            {
+                Console.WriteLine(result.ToString());
+            }
+
+            libpostal.LibpostalAddressParserResponseDestroy(response);
+
+            var expansion = libpostal.LibpostalExpandAddress(query, libpostal.LibpostalGetDefaultOptions());
+            foreach (var s in expansion.Expansions)
+            {
+                Console.WriteLine(s);
+            }
+            
             // Teardown (only called once at the end of your program)
             libpostal.LibpostalTeardown();
             libpostal.LibpostalTeardownParser();
